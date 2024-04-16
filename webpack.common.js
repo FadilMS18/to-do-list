cf92e7a5ebb24f57eaf6483f05203df1a017bf5d
FadilMS18 <muhfadilms2418@gmail.com>
2024-04-16 14:06:23 +0800

    First commit
    added webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports={
+    entry:{
+        main:'./src/index.js'
+    },
+    output:{
+        filename:'[name].bundle.js',
+        path : path.resolve(__dirname, 'dist'),
+        clean: true,
+        assetModuleFilename : 'images/[hash][ext][query]'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'To-Do-List',
+            template:'./src/index.html',
+            meta:{
+                description:'To-Do-List the odin project',
+                author:'FadilMs18',
+            }
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                use:['style-loader', 'css-loader']
+            },
+            {
+                test:/\.(png|jpg|jpeg|gif|svg)/i,
+                type:'asset/resource'
+            },
+            {
+                test:/\.html$/i,
+                loader:'html-loader'
+            }
+        ]
+    }
+}
